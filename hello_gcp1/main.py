435f93605b5194075fe749ba27e2b5dd7dc9680d
Lalit Singh <lalit.singh@kiwitech.com>
2020-12-09 18:20:29 +0530

    env test

--- hello_gcp1/main.py
+++ hello_gcp1/main.py
@@ -1,6 +1,9 @@
+import os
 import requests
 
 def hello_gcp1(request):
-    req = requests.get('https://google.com')
-    return str(req.status_code)
+    # req = requests.get('https://google.com')
+    # return str(req.status_code)
     # return "gcp1 test 103"
+    db_user = os.environ.get("DB_USER", "")
+    return str(db_user)
