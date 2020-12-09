fbf1c0f062cb59efcd4d806c3b64ce0f8b137da7
Lalit Singh <lalit.singh@kiwitech.com>
2020-12-09 19:11:33 +0530

    .env first is done

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # gcp
+
+gcloud kms encrypt \
+  --plaintext-file=.env.yaml \
+  --ciphertext-file=.env.yaml.enc \
+  --location=global \
+  --keyring=Envs \
+  --key=hello-gcp-1-env
+  
--- hello_gcp1/main.py
+++ hello_gcp1/main.py
@@ -5,5 +5,5 @@ def hello_gcp1(request):
     # req = requests.get('https://google.com')
     # return str(req.status_code)
     # return "gcp1 test 103"
-    db_user = os.environ.get("DB_USER", "")
+    db_user = os.environ.get("DB_USER", "No")
     return str(db_user)
